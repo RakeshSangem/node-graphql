f22fc75f8a20d2ee4c0e9c10a27408f3dc38ef6f
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-02-29 12:30:27 +0530

    Add product and order types to GraphQL schema

--- server.js
+++ server.js
@@ -6,23 +6,81 @@ const PORT = process.env.PORT || 3000;
 
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+    type Product {
+      id: ID!
+      description: String!
+      price: Float
+      reviews: [Review]!
+    }
+    type Review {
+      ratin: Int
+      comment: String
+    }
+    type Order {
+      date: String!
+      subtotal: Float!
+      items: [OrderItem]!
+    }
+    type OrderItem {
+      product: Product!
+      quantity: Int!
     }
 `);
 
 const root = {
-  description: 'RedShoe',
-  price: 42.12,
+  products: [
+    {
+      id: '1',
+      description: 'Product 1',
+      price: 100,
+      reviews: [
+        {
+          rating: 5,
+          comment: 'Great product!',
+        },
+      ],
+    },
+    {
+      id: '2',
+      description: 'Product 2',
+      price: 200,
+      reviews: [
+        {
+          rating: 4,
+          comment: 'Good product!',
+        },
+      ],
+    },
+  ],
+  orders: [
+    {
+      date: '2020-01-01',
+      subtotal: 300,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'old red shoe',
+            price: 100,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
 
-app.use(
+app.all(
   '/graphql',
   createHandler({
     schema: schema,
     rootValue: root,
+    graphiql: true,
   })
 );
 
